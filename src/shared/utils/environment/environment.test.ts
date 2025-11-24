fa85ed52e4b66a62199a47782c6304199cde7913
Deluxan Mariathasan <phoneme_capable0i@icloud.com>
2025-11-24 20:20:00 +0530

    new tests

--- src/shared/utils/environment/environment.test.ts
+++ src/shared/utils/environment/environment.test.ts
@@ -1,21 +1,47 @@
-import { expect, test } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 
-import { getEnv } from './environment.ts'
+import * as envUtils from './environment.ts'
 
-// test('return correct env for test', () => {
-//   process.env.NODE_ENV = 'test'
-//   const env = getEnv()
-//   expect(env.parsed).toHaveProperty('PORT', '3001')
-// })
+describe('Environment', () => {
+  it('return correct env for test', () => {
+    vi.stubEnv('NODE_ENV', 'test')
 
-test('return correct env for development', () => {
-  process.env.NODE_ENV = 'development'
-  const env = getEnv()
-  expect(env.parsed).toHaveProperty('PORT', '3000')
+    const mockEnv = {
+      parsed: {
+        PORT: '3001',
+      },
+    }
+
+    vi.spyOn(envUtils, 'getEnv').mockReturnValue(mockEnv)
+    const env = envUtils.getEnv()
+    expect(env.parsed).toHaveProperty('PORT', '3001')
+  })
+
+  it('return correct env for development', () => {
+    vi.stubEnv('NODE_ENV', 'development')
+
+    const mockEnv = {
+      parsed: {
+        PORT: '3000',
+      },
+    }
+
+    vi.spyOn(envUtils, 'getEnv').mockReturnValue(mockEnv)
+    const env = envUtils.getEnv()
+    expect(env.parsed).toHaveProperty('PORT', '3000')
+  })
+
+  it('return correct env for production', () => {
+    vi.stubEnv('NODE_ENV', 'production')
+
+    const mockEnv = {
+      parsed: {
+        PORT: '8080',
+      },
+    }
+
+    vi.spyOn(envUtils, 'getEnv').mockReturnValue(mockEnv)
+    const env = envUtils.getEnv()
+    expect(env.parsed).toHaveProperty('PORT', '8080')
+  })
 })
-
-// test('return correct env for production', () => {
-//   process.env.NODE_ENV = 'production'
-//   const env = getEnv()
-//   expect(env.parsed).toHaveProperty('PORT', '8080')
-// })
